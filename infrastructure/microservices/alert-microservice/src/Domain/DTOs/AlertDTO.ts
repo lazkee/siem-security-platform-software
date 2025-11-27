89f452f28d4a69bf9ac5bfcb72f167b356c1f8e8
vladanaaleksic3 <vladanaaleksic3@gmail.com>
2025-11-27 08:48:06 +0100

    DTOs, enums added for alerts

---
+++ infrastructure/microservices/alert-microservice/src/Domain/DTOs/AlertDTO.ts
@@ -0,0 +1,31 @@
+import { AlertSeverity } from "../enums/AlertSeverity";
+import { AlertStatus } from "../enums/AlertStatus";
+
+// Za vraćanje alerta (GET)
+export interface AlertDTO {
+  id: number;
+  title: string;
+  description: string;
+  severity: AlertSeverity;
+  status: AlertStatus;
+  correlatedEvents: number[]; // Ovdje je niz, u bazi je JSON string
+  source: string;
+  createdAt: Date;
+  resolvedAt: Date | null;
+  resolvedBy: string | null;
+}
+
+// Za kreiranje alerta (POST)
+export interface CreateAlertDTO {
+  title: string;
+  description: string;
+  severity: AlertSeverity;
+  correlatedEvents: number[];
+  source: string;
+}
+
+// Za rješavanje alerta (PUT)
+export interface ResolveAlertDTO {
+  resolvedBy: string;
+  status: AlertStatus;
+}
